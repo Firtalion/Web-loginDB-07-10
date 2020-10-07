89fd4bcba0db431a7d6cee54776994c60883a9fc
Firtalion <oliviermael@gmail.com>
2020-10-07 10:10:11 +0200

    Ajoutez des fichiers projet.

---
+++ Web loginDB 07-10/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_loginDB_07_10.MvcApplication" Language="C#" %>
---
+++ Web loginDB 07-10/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_loginDB_07_10.Models
+{
+    public class Student
+    {
+        public Guid StudentId { get; set; }
+        [Required]
+        public string Nom { get; set; }
+        [Required]
+        public string Prenom { get; set; }
+        [Required]
+
+        public string Couriel { get; set; }
+        [Required]
+
+        public DateTime DateNaissance { get; set; }
+        [Required]
+
+        public string Matricule { get; set; }
+
+        public string GSM { get; set; }
+    }
+}
